9b05440f0300f07335220030c26dee75871fa636
FrancStyven <125910260+FrancStyven@users.noreply.github.com>
2025-01-22 10:35:03 -0400

    Update StatusPACO.js

--- StatusPACO.js
+++ StatusPACO.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded",()=>{
+    var datos  = [];
+    var objeto = {};
+    
     datos.push({
         "action1": "1",
         "action2": "0"
-        
     })
+    objeto.datos = datos;
+    console.log(JSON.stringify(objeto));
 });
         
